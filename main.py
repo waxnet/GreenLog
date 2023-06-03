790f4fd812b55ea00d6f8be769e199c321e37bde
waxnet <88239635+waxnet@users.noreply.github.com>
2023-06-04 00:15:28 +0200

    added static dump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utilities import (
-    file_system,
+    enviroment,
     process,
     banner,
     data,
@@ -22,91 +22,137 @@ class green_log():
         time.sleep(1)
     
     def main(self):
-        # request amount of seconds before starting the dumping phase
-        assets_loading_process_timeout = io.inp("Enter a timeout in seconds for the assets loading process (10 min, 30 max)")
-        if assets_loading_process_timeout != "":
-            try:
-                assets_loading_process_timeout = int(assets_loading_process_timeout)
+        # check if temp files already exist
+        mode = ""
+        if enviroment.check_path(data.http_folder):
+            mode = io.inp("Would you like to perform a static dump? (y, n)").lower()
 
-                # limit the amount of seconds
-                if assets_loading_process_timeout < 10:
-                    assets_loading_process_timeout = 10
-                elif assets_loading_process_timeout > 30:
-                    assets_loading_process_timeout = 30
+        ## static dump
+        if mode == "y":
+            # starting message
+            io.out("\nStarting static dump...")
+            io.out("")
 
-                # set timer selected timeout and current seconds
-                data.selected_timeout = assets_loading_process_timeout
-                data.timeout = assets_loading_process_timeout
-            except ValueError:
-                pass
-        io.out("\n")
+            # clean, setup and prepare folders for dumping
+            io.out("Setting up folders...")
+            enviroment.set_folders([
+                data.downloads_dump,
+                data.sounds_dump,
+                data.http_dump,
+                
+                data.sounds_output,
+                data.http_images,
+                data.http_audios,
+            ])
+
+            # kill Roblox process if running
+            roblox_process = process.find_process("RobloxPlayerBeta.exe")
+            if roblox_process:
+                io.out("Killing Roblox process...")
+                roblox_process.kill()
+                roblox_process.wait()
+
+            # dump data
+            io.out("Dumping data...")
+            enviroment.dump_folder(data.sounds_folder, data.sounds_dump)
+            enviroment.dump_folder(data.http_folder, data.http_dump)
+            
+            # parse data and extract assets
+            io.out("Parsing data... (this might take a couple of minutes)")
+            enviroment.parse_folder(data.sounds_dump)
+            enviroment.parse_folder(data.http_dump)
         
-        # kill Roblox process if running
-        roblox_process = process.find_process("RobloxPlayerBeta.exe")
-        if roblox_process:
-            io.out("Killing already running Roblox process...")
+        ## dynamic dump
+        else:
+            # starting message
+            io.out("\nStarting dynamic dump...")
+            io.out("")
+
+            # request amount of seconds before starting the dumping phase
+            assets_loading_process_timeout = io.inp("Enter a timeout in seconds for the assets loading process (10 min, 30 max)")
+            if assets_loading_process_timeout != "":
+                try:
+                    assets_loading_process_timeout = int(assets_loading_process_timeout)
+
+                    # limit the amount of seconds
+                    if assets_loading_process_timeout < 10:
+                        assets_loading_process_timeout = 10
+                    elif assets_loading_process_timeout > 30:
+                        assets_loading_process_timeout = 30
+
+                    # set timer selected timeout and current seconds
+                    data.selected_timeout = assets_loading_process_timeout
+                    data.timeout = assets_loading_process_timeout
+                except ValueError:
+                    pass
+            io.out("")
+            
+            # kill Roblox process if running
+            roblox_process = process.find_process("RobloxPlayerBeta.exe")
+            if roblox_process:
+                io.out("Killing already running Roblox process...")
+                roblox_process.kill()
+                roblox_process.wait()
+            
+            # clean, setup and prepare folders for dumping
+            io.out("Setting up folders...")
+            enviroment.set_folders([
+                data.sounds_folder,
+                data.http_folder,
+                
+                data.downloads_dump,
+                data.sounds_dump,
+                data.http_dump,
+                
+                data.sounds_output,
+                data.http_images,
+                data.http_audios
+            ])
+            
+            # setup file observers (used to check if new assets are loaded)
+            io.out("Starting file observers...")
+            sounds_observer = enviroment.set_observer(data.sounds_folder)
+            http_observer = enviroment.set_observer(data.http_folder)
+            sounds_observer.start()
+            http_observer.start()
+            
+            # wait for Roblox process to start
+            io.out("You can now open Roblox, waiting for Roblox...")
+            roblox_process = process.wait_for_process("RobloxPlayerBeta.exe")
+            
+            # start the timer
+            io.out("Starting timer... (the timer will reset every time a new asset is loaded)")
+            while data.timeout > 0:
+                cmd.execute([f"title GreenLog - {data.timeout} seconds left - {data.resets} timer resets"])
+                data.timeout -= 1
+                time.sleep(1)
+            cmd.execute(["title GreenLog"])
+            
+            # stop file observers
+            io.out("The timer has reached its timeout, stopping file observers...")
+            sounds_observer.stop()
+            http_observer.stop()
+            sounds_observer.join()
+            http_observer.join()
+            
+            # kill Roblox process
+            io.out("Killing Roblox process...")
             roblox_process.kill()
-            roblox_process.wait()
-        
-        # clean, setup and prepare folders for dumping
-        io.out("Setting up folders...")
-        file_system.set_folders([
-            data.sounds_folder,
-            data.http_folder,
             
-            data.downloads_dump,
-            data.sounds_dump,
-            data.http_dump,
+            # dump loaded data
+            io.out("Dumping data...")
+            enviroment.dump_folder(data.sounds_folder, data.sounds_dump)
+            enviroment.dump_folder(data.http_folder, data.http_dump)
             
-            data.sounds_output,
-            data.http_images,
-            data.http_audios
-        ])
-        
-        # setup file observers (used to check if new assets are loaded)
-        io.out("Starting file observers...")
-        sounds_observer = file_system.set_observer(data.sounds_folder)
-        http_observer = file_system.set_observer(data.http_folder)
-        sounds_observer.start()
-        http_observer.start()
-        
-        # wait for Roblox process to start
-        io.out("You can now open Roblox, waiting for Roblox...")
-        roblox_process = process.wait_for_process("RobloxPlayerBeta.exe")
-        
-        # start the timer
-        io.out("Starting timer... (the timer will reset every time a new asset is loaded)")
-        while data.timeout > 0:
-            cmd.execute([f"title GreenLog - {data.timeout} seconds left - {data.resets} timer resets"])
-            data.timeout -= 1
-            time.sleep(1)
-        cmd.execute(["title GreenLog"])
-        
-        # stop file observers
-        io.out("The timer has reached its timeout, stopping file observers...")
-        sounds_observer.stop()
-        http_observer.stop()
-        sounds_observer.join()
-        http_observer.join()
-        
-        # kill Roblox process
-        io.out("Killing Roblox process...")
-        roblox_process.kill()
-        
-        # dump loaded data
-        io.out("Dumping data...")
-        file_system.dump_folder(data.sounds_folder, data.sounds_dump)
-        file_system.dump_folder(data.http_folder, data.http_dump)
-        
-        # parse data and extract assets
-        io.out("Parsing data... (this might take a couple of minutes)")
-        file_system.parse_folder(data.sounds_dump)
-        file_system.parse_folder(data.http_dump)
+            # parse data and extract assets
+            io.out("Parsing data... (this might take a couple of minutes)")
+            enviroment.parse_folder(data.sounds_dump)
+            enviroment.parse_folder(data.http_dump)
         
         io.out("\nDone! Check the \".output\" folder to see the extracted files.")
         io.out("Press any key to exit...")
         keyboard.read_key()
-        exit()
+        exit(0)
 
 if __name__ == "__main__":
     green_log()
---
+++ utilities/enviroment.py
@@ -0,0 +1,89 @@
+from utilities import (
+    http,
+    data
+)
+import watchdog.observers
+import watchdog.events
+import shutil
+import magic
+import os
+
+# file events for observers
+class file_event_handler(watchdog.events.LoggingEventHandler):
+    def on_created(self, _):
+        data.timeout = data.selected_timeout
+        data.resets += 1
+
+# setup paths
+def set_folders(paths):
+    for path in paths:
+        if not os.path.exists(path):
+            os.makedirs(path)
+        else:
+            for file in os.listdir(path):
+                os.remove(os.path.join(path, file))
+
+# setup observer
+def set_observer(path):
+    observer = watchdog.observers.Observer()
+    observer.schedule(file_event_handler(), path, recursive=True)
+    return observer
+
+# dump folder content into another folder
+def dump_folder(path, destination):
+    for file in os.listdir(path):
+        shutil.copy2(os.path.join(path, file), destination)
+
+# check if path exists
+def check_path(path):
+    return os.path.exists(path)
+
+# parse dumped data
+def parse_folder(path):
+    sound_counter = 0 # sound counter
+    audio_counter = 0 # audio counter
+    image_counter = 0 # image counter
+
+    for file in os.listdir(path):
+        file_path = os.path.join(path, file)
+        
+        if not file.startswith("RBX"): # check file name
+            with open(file_path, "r", errors="ignore") as data_file:
+                data_file_first_line = data_file.readline()
+                data_file_content = data_file.read()
+                data_file.close()
+            
+            if "binary/octet-stream" in data_file_content: # check if file is valid
+                file_url = http.build_url(data_file_first_line) # build the Roblox CDN url for the asset
+                file_content = http.get_content(file_url) # download the asset
+                
+                if file_content: # check if file was downloaded successfully
+                    # write file to the download folder
+                    downloaded_file_path = os.path.join(data.downloads_dump, file)
+                    with open(downloaded_file_path, "wb") as downloaded_file:
+                        downloaded_file.write(file_content)
+                        downloaded_file.close()
+                    
+                    # get and check file type
+                    file_type = magic.from_file(downloaded_file_path)
+                    if "audio" in file_type:
+                        new_downloaded_file_directory = os.path.join(data.http_audios, f"{audio_counter}.ogg") # write file to the http audio folder
+                        os.rename(downloaded_file_path, new_downloaded_file_directory)
+                        audio_counter += 1
+                    elif "image" in file_type:
+                        new_downloaded_file_directory = os.path.join(data.http_images, f"{image_counter}.jpg") # write file to the http image folder
+                        os.rename(downloaded_file_path, new_downloaded_file_directory)
+                        image_counter += 1
+                    else:
+                        continue
+        else:
+            # rewrite file as ogg in the sounds output folder
+            with open(file_path, "rb") as data_file:
+                data_file_first_line = data_file.read()
+                data_file.close()
+            
+            with open(os.path.join(data.sounds_output, f"{sound_counter}.ogg"), "wb") as parsed_file:
+                parsed_file.write(data_file_first_line)
+                parsed_file.close()
+            
+            sound_counter += 1 # update sound counter
